57d82935d1d63cff0948ffadada894f222de4f3c
David Robert <contact@david-robert.fr>
2023-12-11 14:21:48 +0100

    use effect

--- src/page/MealsPage.jsx
+++ src/page/MealsPage.jsx
@@ -1,33 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const MealsPage = () => {
-  // au chargement du composant, je créé un state
-  // nommé meals, contenant par défaut null
-  // le state est une sorte de BDD pour le composant
-  // use state me retourne deux chose : la valeur
-  // actuelle du state de meals, et une fonction pour la
-  // modifier. La fonction permet en plus de recharger le composant
-
-  // quand mon composant sera rechargé, il va d'abord
-  // regarder si une donnée existe pour le state meals
-  // si oui il va l'utiliser, sinon il utilisera la valeur null
   const [meals, setMeals] = useState(null);
 
-  if (meals === null) {
+  // use Effect permet d'executer une fonction
+  // à certains rendus du composant
+  // il prend en paramètre la fonction à executer
+  // et en second parametre : quand elle doit être executée
+  // si pas de second parametre : la fn sera executée à chaque chargement
+  // du composant
+  // si second parametre === tableau vide : la fn sera executée une seule fois
+  // après le premier chargement du composant
+  useEffect(() => {
     (async () => {
       const mealsResponse = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=");
-      // quand les données sont récupérées depuis mon api
-      // je stocke dans le state les données de l'api
-      // ce qui force mon composant à être rechargé
-      setMeals(await mealsResponse.json());
+      const mealsResponseData = await mealsResponse.json();
+
+      setMeals(mealsResponseData.meals);
     })();
-  }
+  }, []);
 
   return (
     <div>
       {meals ? (
         <>
-          {meals.meals.map((meal) => {
+          {meals.map((meal) => {
             return (
               <article>
                 <p>{meal.strMeal}</p>
